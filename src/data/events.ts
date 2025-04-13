c950fe578321d12b8c7ad13e47dcfe3a7bf00434
Alberto Bautista <dachb101@gmail.com>
2025-04-13 15:51:06 -0600

    update events data

--- src/data/events.ts
+++ src/data/events.ts
@@ -6,21 +6,24 @@ const events: EventData[] = [
     title: "Birthday",
     description: "Cumpleaños - 2025",
     timestamp: 1765087200000,
-    backgroundImage: "/img/bg/akron.jpg",
+    backgroundImage: "/img/bg/akron.webp",
+    colorClass: "text-red-600",
   },
   {
     id: "los-angeles",
     title: "Los Angeles 🇲🇽",
     description: "Los Angeles 2025",
     timestamp: 1749949200000,
-    backgroundImage: "/img/bg/sofi.jpg",
+    backgroundImage: "/img/bg/sofi.webp",
+    colorClass: "text-green-600",
   },
   {
     id: "final-kings-league",
     title: "Final Kings League",
     description: "Final Kings League - 2025",
     timestamp: 1747530000000,
-    backgroundImage: "/img/bg/kings2.jpeg",
+    backgroundImage: "/img/bg/kings.webp",
+    colorClass: "text-yellow-500",
   },
 ];
 
